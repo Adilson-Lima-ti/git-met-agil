2c37bca4e0e2bb6e2e457109883041a4fa246da2
Adilson Lima <adilson.monte.alto@gmail.com>
2022-01-15 11:44:16 -0300

    feat: Implementing start in project

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import random
 import time
+import random
 from messages.messages import display_messages
 
-print ('Starting Project!!')
+print ('Starting Project again!!')
 time.sleep(2)
 while True:
     resposta = input('Deseja receber um Conselho? S/N: ')
