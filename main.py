ebe87cdd97a093789275837f5b5ec5db23fad93d
Adilson Lima <adilson.monte.alto@gmail.com>
2022-01-15 11:21:12 -0300

    feat: Implementing Presenation before project start 2"

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 import time
 from messages.messages import display_messages
 
-print ('Starting Project!')
-time.sleep(3)
+print ('Starting Project!!')
+time.sleep(2)
 while True:
     resposta = input('Deseja receber um Conselho? S/N: ')
     if (resposta == 'S' or resposta == 's'):
